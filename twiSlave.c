29e416d220296e4a055614e2b1b5da1dbc2a8c82
Winggel <hazaelsb97@gmail.com>
2017-08-28 19:33:10 -0300

    Add TWI module for ATmega328P, slave and master. Slave update function not implemented, since it needs adjustments to be consistent across libraries.

---
+++ twiSlave.c
@@ -0,0 +1,137 @@
+/* -----------------------------------------------------------------------------
+ * Project:			GPDSE AVR8 Library
+ * File:			twiSlave.c
+ * Module:			TWI Slave controller
+ * Author:			Leandro Schwarz
+ *					Hazael dos Santos Batista
+ * Version:			1.0
+ * Last edition:	August 28, 2017
+ * Purpose:			Interfaces a TWI Slave data bus
+ * -------------------------------------------------------------------------- */
+
+// -----------------------------------------------------------------------------
+// Header files ----------------------------------------------------------------
+
+#include "twiSlave.h"
+#if __TWI_SLAVE_H != 10
+#error Error 101 - Version mismatch on header and source code files (twiSlave).
+#endif
+
+// -----------------------------------------------------------------------------
+// Global variables ------------------------------------------------------------
+
+static twiBuffer_t * twiBufferData;
+static volatile uint8 twiBufferIndex = 0;
+static volatile uint8 twiBufferSize = 0;
+static volatile uint8 twiCommIndex = 0;
+static volatile uint8 twiBusy = 0;
+
+// -----------------------------------------------------------------------------
+// Public function definitions -------------------------------------------------
+
+twiBuffer_t * twiSlaveInit(uint8 twiSlaveAddr, uint8 bufferSize, bool_t genCallAcceptance)
+{
+    TWAR = (twiSlaveAddr << 1) | (genCallAcceptance & 1);
+    TWCR =	(1 << TWEN) |
+            (1 << TWIE) | (1 << TWINT) |
+            (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+            (0 << TWWC);
+    twiBusy = 0;
+
+    twiBufferData = (twiBuffer_t *)calloc(bufferSize, sizeof(uint8));
+
+    if(twiBufferData == NULL) {
+        return NULL;
+    }
+
+    twiBufferSize = bufferSize;
+
+    return twiBufferData;
+}
+
+ISR(TWI_vect)
+{
+    switch (TWSR) {
+    case TWI_STX_ADR_ACK:
+    case TWI_STX_DATA_ACK:
+        TWDR = twiBufferData[twiBufferIndex++];
+
+        if(twiBufferIndex >= twiBufferSize) {
+            twiBufferIndex = 0;
+        }
+
+        TWCR =	(1 << TWEN) |
+                (1 << TWIE) | (1 << TWINT) |
+                (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+                (0 << TWWC);
+        twiBusy = 1;
+        break;
+
+    case TWI_STX_DATA_NACK:
+        TWCR =	(1 << TWEN) |
+                (1 << TWIE) | (1 << TWINT) |
+                (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+                (0 << TWWC);
+        twiBusy = 0;
+        break;
+
+    case TWI_SRX_GEN_ACK:
+    case TWI_SRX_ADR_ACK:
+        TWCR =	(1 << TWEN) |
+                (1 << TWIE) | (1 << TWINT) |
+                (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+                (0 << TWWC);
+        twiCommIndex = TRUE;
+        twiBusy = 1;
+        break;
+
+    case TWI_SRX_ADR_DATA_ACK:
+    case TWI_SRX_GEN_DATA_ACK:
+        if(twiCommIndex == TRUE) {
+            twiCommIndex = FALSE;
+            twiBufferIndex = TWDR;
+        }
+        else {
+            twiBufferIndex++;
+
+            if(twiBufferIndex >= twiBufferSize) {
+                twiBufferIndex = 0;
+            }
+        }
+
+        TWCR =	(1 << TWEN) |
+                (1 << TWIE) | (1 << TWINT) |
+                (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+                (0 << TWWC);
+        twiBusy = 1;
+        break;
+
+    case TWI_SRX_STOP_RESTART:
+        TWCR =	(1 << TWEN) |
+                (1 << TWIE) | (1 << TWINT) |
+                (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+                (0 << TWWC);
+        twiBusy = 0;
+        break;
+
+    case TWI_SRX_ADR_DATA_NACK:
+    case TWI_SRX_GEN_DATA_NACK:
+    case TWI_STX_DATA_ACK_LAST_BYTE:
+    case TWI_BUS_ERROR:
+        TWCR =	(0 << TWEN) |
+                (0 << TWIE) | (1 << TWINT) |
+                (0 << TWEA) | (0 << TWSTA) | (1 << TWSTO) |
+                (0 << TWWC);
+        twiBusy = 0;
+
+        break;
+
+    default:
+        TWCR =	(1 << TWEN) |
+                (1 << TWIE) | (1 << TWINT) |
+                (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) |
+                (0 << TWWC);
+        twiBusy = 0;
+        break;
+    }
+}
